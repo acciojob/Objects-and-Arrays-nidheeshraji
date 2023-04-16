410a5ce633d924bf4d886fd030bdd18292ada9dd
nidheeshraji <nidheeshraji111@gmail.com>
2023-04-16 08:35:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+// Create reference to players array
+const team = players;
+
+// Create copy of players array
+const team1 = [...players];
+
+// Create copy of person object
+const cap1 = { ...person };
+
+// Print the variables
+console.log('Team:', team);
+console.log('Team1:', team1);
+console.log('Cap1:', cap1);
+
 
 window.players = players;
 window.person = person;
